b2cb9dcd1c2b8b254ecb7793e7674724b755ad2b
Muhammed-Sirajudeen <muhammedsirajudeen29@gmail.com>
2023-02-17 23:01:49 +0530

    added the layout sorter frontend now just need to add the backend logic

--- allocator-backend/backend.py
+++ allocator-backend/backend.py
@@ -1,11 +1,10 @@
-
 from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 import pymongo
+import pandas as pd
 
 app = Flask(__name__)
-
 api = Api(app)
 CORS(app)
 
@@ -22,58 +21,58 @@ class DatabaseConnection():
 		myclient = pymongo.MongoClient(self.url)
 		mydb = myclient[self.dbname]
 		mycol=mydb[self.collection]
-		
 		return mycol
 
 try:
-
 	DBCONNECTION=DatabaseConnection("seatdb","layouts")
 	DBCOLLECTION=DBCONNECTION.connection()
-	
 	print("connection to the database is success")
 except:
 	print("connection to the database failed")
-class Hello(Resource):
 
 
+class LayoutPost(Resource):
 	def get(self):
+		try:
+			mydoc = DBCOLLECTION.find()
+			responsedata=[]
+			for x in mydoc:
+				responsedata.append(x["classnumber"])
 
-		return jsonify({'message': 'hello world'})
+			return jsonify({'status': '200OK',"data":responsedata,"error":None})
+		except:
+			return jsonify({'status':"503","error":"network error"})
 
 	def post(self):
 		try:
 			data = request.get_json()
 			uniqueflag=False #the variable to check if duplicate data is present
-
 			myquery = { "classnumber": data["classnumber"] }
 			mydoc = DBCOLLECTION.find(myquery)
-			
 			for collectionData in mydoc:
 				uniqueflag=True
-			
 			if(uniqueflag):
 				'''
 					only post data to the database if there doesnt exist a duplicate data
 				'''
-				# x = DBCONNECTION.insert_one(data)
 				return {"status":"200OK","error":"duplicate"},201
 			else:
 				x = DBCOLLECTION.insert_one(data)
 				return {"status":"200OK","error":None},201
 		except:
-			return {"status":"403NOTOK","error":"internal network error"},201		
-		
+			return {"status":"403NOTOK","error":"internal network error"},503		
 
+class ExamSorter(Resource):
+	def get(self):
+		return {"status":"200OK","error":None}
+	
 
+	def post(self):
+		data = request.get_json()
 
+		return {"status":"200OK","error":None}
 
-
-
-api.add_resource(Hello, '/')
-
-
-
-
+api.add_resource(LayoutPost, '/')
+api.add_resource(ExamSorter,'/sort')
 if __name__ == '__main__':
-
 	app.run(debug = True)
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,101 @@
-import React from 'react'
- 
+import React, { useEffect } from 'react'
+import "./stylesheets/Home.css"
+import { useState } from 'react'
+import Select from 'react-select'
+import makeAnimated from 'react-select/animated';
+
+import axios from 'axios'
+
+const animatedComponents = makeAnimated();
+
+
 function Home() {
+  const [classNumbers,setclassNumbers]=useState([])
+  const [selectNumber,setselectNumber]=useState([])
+  const [subjectCode,setsubjectCode]=useState([])
+  const [subjectElements,setsubjectElements]=useState([])
+  
+  useEffect(()=>{
+    let options = []
+    async function loadclassnumber(){
+      let data=await axios.get("http://localhost:5000")
+      let classnumber=data.data.data //i know it seems stupid yet it seems fun
+      classnumber.forEach((data)=>{
+        options.push({value:data,label:data})
+      })
+      setclassNumbers(options)
+
+    }
+    loadclassnumber()
+  },[])
+  function selecthandler(value){
+    
+    setselectNumber(value)
+
+  }
+  function subjecthandler(element){
+    setsubjectCode(element.target.value)
+
+  }
+  function subjsubmitHandler(){
+    
+    setsubjectElements([...subjectElements,subjectCode])
+    setsubjectCode("")
+  }
+  function codebuttonHandler(element){
+    
+    
+    let codes=subjectElements.filter((code)=>{
+      return code!==element.target.id
+    })
+    
+    setsubjectElements(codes)
+  }
+
+  function retrieveHandler(){
+
+    let classnumbers=[]
+    selectNumber.forEach((data)=>{  
+      classnumbers.push(data.value)
+                 
+    })
+    console.log(classnumbers) //do not confuse with the state variable classnumbers
+    console.log(subjectElements) //we have the data here now we just have to post the data to the backend and we are good to go
+  }
+
   return (
-    <div>hello world</div>
+    <div className='main-container'>
+           <Select className='select-box'
+      closeMenuOnSelect={false}
+      components={animatedComponents}
+      onChange={selecthandler}
+      isMulti
+      options={classNumbers}
+    />
+
+    <div className='input-container'>
+      <input type='text' className='subjectinput' placeholder='enter subject code' onChange={subjecthandler} value={subjectCode}/>
+      <button className='submit' onClick={subjsubmitHandler}>add code</button>
+    </div>
+    <div className='code-container'>
+      {subjectElements.map((value)=>{
+        return(
+          <div className='code-sub-container' key={value}>
+            <div>{value}</div>
+            <button id={value} className='code-button' onClick={codebuttonHandler}>x</button>
+          </div>
+        )
+      })}
+    </div>  
+
+    <button className='retrieve-button' onClick={retrieveHandler}>
+      Retrieve Layout
+    </button>
+
+
+
+
+    </div>
   )
 }
 
